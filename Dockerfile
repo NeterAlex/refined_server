5d73aa033fdf3030de7c5f2d908ad6c656faee35
neteralex <neteralex@outlook.com>
2023-05-12 15:40:19 +0800

    modified

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM golang:alpine
+MAINTAINER NeterAlex "neteralex@foxmail.com"
+
+ENV GO111MODULE=on \
+    CGO_ENABLE=0 \
+    GOOS=linux \
+    GOARCH=amd64 \
+    GOPROXY="https://goproxy.cn,direct"
+
+WORKDIR /refined_server
+#COPY biz/config/config.yml ./app/biz/config/
+COPY biz/config/config.yml ./config/
+COPY biz/dal/database/refined.db ./biz/dal/database/
+COPY out/* ./
+
+EXPOSE 8022
+RUN chmod +xwr /refined_server/
+ENTRYPOINT ["/refined_server/go_build_linux_amd64_linux"]
--- biz/config/init.go
+++ biz/config/init.go
@@ -3,18 +3,21 @@ package config
 import (
 	"fmt"
 	"github.com/spf13/viper"
+	"os"
 )
 
 func Init() {
 	var err error
 	viper.AddConfigPath("./config")
 	viper.SetConfigType("yml")
-	viper.SetConfigName("RefinedConfig")
-	viper.SetConfigFile("biz/config/config.yml")
+	viper.SetConfigName("config")
+	dir, err := os.Getwd()
+	fmt.Println(dir)
 	viper.SetDefault("server.host", "localhost")
 	viper.SetDefault("server.port", "8022")
 	viper.SetDefault("database.sqlite.file", "biz/dal/database/refined.db")
 	viper.SetDefault("jwt.secretkey", "F54d|CX1v")
+
 	err = viper.ReadInConfig()
 	if err != nil {
 		panic(fmt.Errorf("Fatal error in reading config file: %s\n", err))
